128f66025ceda75e06de180ed7e20d2bd81eb0c0
XieLiang <liang.x@taboola.com>
2024-11-28 19:25:36 +0800

    fix

--- src/VideoSwipe.tsx
+++ src/VideoSwipe.tsx
@@ -4,21 +4,19 @@ import { useSwipeable } from "react-swipeable";
 
 const VideoSwipe: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [isClient, setIsClient] = useState<boolean>(false); 
+  const [isClient, setIsClient] = useState<boolean>(false);
 
   const videos: string[] = ["/video1.mp4", "/video2.mp4", "/video3.mp4"];
 
-  // 是否已经滑动到第一个或最后一个视频
   const [isAtStart, setIsAtStart] = useState<boolean>(false);
   const [isAtEnd, setIsAtEnd] = useState<boolean>(false);
+
 
   // 滑动事件处理函数
   const handleSwipe = (direction: string) => {
     if (direction === "Up" && !isAtEnd) {
-      // 向上滑动，切换到下一个视频
       setCurrentIndex((prevIndex) => (prevIndex + 1) % videos.length);
     } else if (direction === "Down" && !isAtStart) {
-      // 向下滑动，切换到上一个视频
       setCurrentIndex(
         (prevIndex) => (prevIndex - 1 + videos.length) % videos.length
       );
@@ -33,7 +31,6 @@ const VideoSwipe: React.FC = () => {
     trackMouse: false,
   });
 
-  // 监听当前视频的索引，更新是否达到边界状态
   useEffect(() => {
     setIsAtStart(currentIndex === 0);
     setIsAtEnd(currentIndex === videos.length - 1);
@@ -49,7 +46,6 @@ const VideoSwipe: React.FC = () => {
 
   return (
     <div className="video-container" {...handlers}>
-      {/* 黑色背景显示 */}
       {isAtStart && (
         <div className="black-background"></div> // 当到达第一个视频时显示背景
       )}
@@ -75,13 +71,13 @@ const VideoSwipe: React.FC = () => {
         >
           <video
             src={video}
-            autoPlay={index === currentIndex}
             muted
             loop
+            autoPlay
+            controls
             style={{
               width: "100%",
               height: "100%",
-              //   objectFit: "cover",
             }}
           />
         </motion.div>
